8d6bb67f6040df1658da6838be20a7b7b6330150
Jayson Broughton <jayson.broughton@gmail.com>
2020-12-29 09:16:15 +0000

    Git went a bit weird. Pulling dev adds master root directory. Reverting removes. Whacking this (contains master) in hope that it doesn't delete all my devl code

--- image_parser/scripts/keygen.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-## Build Keys
-mkdir ../keys
-ssh-keygen -t rsa -N "" -f ../keys/console
-ssh-keygen -t rsa -N "" -f ../keys/database
-
--- image_parser/terraform/destroy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd devops/
-terraform destroy -auto-approve 
-cd ../platform
-terraform destroy -auto-approve 
-#cd ../global/buckets
-#terraform destroy -auto-approve global/buckets/
